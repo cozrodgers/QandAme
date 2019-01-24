1cf0fd246c7a1a1a6e7d8fda2a488c04210b4811
Corey Rodgers <coreyrodgers95@gmail.com>
2019-01-24 19:57:57 +0000

    added scrollable div window and view border

--- src/components/questionView.js
+++ src/components/questionView.js
@@ -1,6 +1,6 @@
 import React, {Component}  from 'react';
 import Item from './item.js';
-//import './questionView.css';
+import './questionView.css';
 
 
 
@@ -13,15 +13,13 @@ class QuestionView extends Component {
 
 		return (
 
-		<div ClassName="view">
+		<div className="view">
 	<ul>
  <Item type="answer" user="s1" text="Awesome!"/>
         <Item type="suestion" user="s2" text="Hello what is your favourite color"/>
            <Item type="answer" user="s3" text="Blue"/>
            <Item type="answer" user="s3" text="and Green"/>
            <Item type="answer" user="s3" text="and Green"/>
-
-
 	</ul>
 		</div>
 
