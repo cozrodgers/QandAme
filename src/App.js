a6bfee995c1e95654dbd5dcc0d8e91af4898d5a6
Corey Rodgers <coreyrodgers95@gmail.com>
2019-01-29 06:59:52 -0800

    Add 'Footer' and Add 'Voting Buttons'
    added a footer and now began to add voting buttons, I will try to write a function that should take in a mouse click and move the message up

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./components/hello.js";
 import AskQuestion from "./components/askQuestion.js";
 import QuestionView from "./components/questionView.js";
 import Item from "./components/item.js";
 import Menu from "./components/menu.js";
-import mockItems from './items'
+import Footer from "./components/footer.js";
+import mockItems from "./items";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {items : []}
+  constructor() {
+    super();
+    this.state = { items: [] };
   }
 
-  componentWillMount(){
-    this.setState({items:mockItems})
+  componentWillMount() {
+    this.setState({ items: mockItems });
   }
 
   handleAddItem(item) {
     let items = this.state.items;
-    items.push(item)
-    this.setState({items: items})
+    items.push(item);
+    this.setState({ items: items });
   }
-
-
 
   render() {
     return (
       <div className="App">
         <Hello />
 
-        <QuestionView itemlist = {this.state.items}/>
+        <QuestionView itemlist={this.state.items} />
 
-        <AskQuestion addItem = {this.handleAddItem.bind(this)}/>
+        <AskQuestion addItem={this.handleAddItem.bind(this)} />
+
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./footer.css";
+import "./tachyons.min.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="all">
+        <p className="dev">Developed by Corey Rodgers and Imad Baddin</p>
+        <p className="social">Find us on Github & LinkedIn</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
