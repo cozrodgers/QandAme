bb4f203361ce3f9c3ac5e34b13f1bc05bb0ca149
Corey Rodgers <coreyrodgers95@gmail.com>
2019-02-11 18:49:17 +0000

    add mockitems to api folder and configure server

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 // require modules
 const express = require('express');
 const app = express();
+const items = require('./items.js');
 
 //call the instance and use the get function of the instance
 app.get('/', function (req, res) {
     //set content type as json
 res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ username: "user1", email: "test@test.com" }));
+  res.send(JSON.stringify(items));
 });
 
 //listen on port 8080
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8084);
