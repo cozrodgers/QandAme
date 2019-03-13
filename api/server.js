fdf71117fc006aff97f202ccf0be4b17060fb9be
Corey Rodgers <coreyrodgers95@gmail.com>
2019-03-13 02:44:02 +0000

    add CRUD endpoints to server.js

--- api/server.js
+++ api/server.js
@@ -4,18 +4,40 @@ const express = require('express');
 
 const app = express();
 
-
-// var cors = require('cors');
-// app.use(cors());
+//require cors
+var cors = require('cors');
+app.use(cors());
 
 const items = require('./items');
 
+
+
 //call the instance and use the get function of the instance
 app.get('/', function (req, res) {
-    //set content type as json
+
+//set content type as json
 res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(items));
 });
+
+
+
+//start endpoints here 
+
+app.post('/items', create); //create a new item in /items
+
+
+app.get('/items', findAll); //retrieve all items from items
+
+
+app.get('/items/:itemId', findOne); //retrieve a single item based on its id
+
+
+app.put('/items/:itemId', update); //change an item contents based on it sid
+
+
+app.delete('/items/:itemId', deleteItem); //delete an item based on its id
+
 
 //listen on port 8080
 app.listen(process.env.PORT || 8085);
